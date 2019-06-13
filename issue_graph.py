d3d57ce27b531f2bb33a123f60e8b1a6c202a431
Alexander Egorov <mofr@zond.org>
2019-06-13 18:56:06 +0700

    Update graph view

--- issue_graph.py
+++ issue_graph.py
@@ -7,20 +7,26 @@ from graphviz import Digraph
 
 class IssueGraph:
     issue_labels_html = {
-        'Frontend': '<font color="#78c48c">Front</font>',
-        'Backend': '<font color="#6268a3">Back</font>',
+        'Frontend': '<b><font color="#78c48c">Front</font></b>',
+        'Backend': '<b><font color="#6268a3">Back</font></b>',
+        'backend': '<b><font color="#6268a3">Back</font></b>',
     }
 
     def __init__(self):
         self.graph = Digraph(comment='Dependency graph')
         self.graph.node_attr.update(shape='box')
+        self.graph.graph_attr.update(rankdir='LR')
 
-    def add_issue(self, id_: str, title: str, points: str, labels: List[str] = ()):
-        short_title = title[:10]
+    def add_issue(self, id_: str, summary: str, points: str, status: str, labels: List[str] = ()):
+        short_summary = summary[:50] if summary else ''
         first_line = f'<b>{id_}</b>'
         for label in labels:
             first_line += ' ' + self.issue_labels_html.get(label, label)
-        node_label = f'{first_line}<br/>{short_title}<br/>{points} points'
+        node_label = f'{first_line}<br/>{short_summary}'
+        if points:
+            node_label += f'<br/>Story points: {int(points)}'
+        else:
+            node_label += f'<br/><b><font color="#ff0000">No estimation</font></b>'
         self.graph.node(name=id_, label=f'<{node_label}>')
 
     def add_link(self, blocker: str, blocked: str):
--- jira_client.py
+++ jira_client.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from collections import defaultdict
 from typing import List
 
 from jira import Issue
@@ -17,11 +18,42 @@ epic_keys = sys.argv[1:]
 jql = ' OR '.join(f'"Epic Link"={epic_key}' for epic_key in epic_keys)
 issues: List[Issue] = auth_jira.search_issues(jql, maxResults=False)
 
+
+def is_closed(issue) -> bool:
+    return issue.fields.status.name not in ('In Progress', 'Opened')
+
+
+total_points = 0
+total_points_per_label = defaultdict(int)
+not_estimated_issues = []
 issue_graph = IssueGraph()
 for issue in issues:
-    issue_graph.add_issue(issue.key, issue.key, '1')
+    if is_closed(issue):
+        continue
+    story_points = issue.fields.customfield_10005
+    status_name = issue.fields.status.name
+    issue_graph.add_issue(
+        issue.key,
+        summary=issue.fields.summary,
+        points=story_points,
+        status=status_name,
+        labels=issue.fields.labels,
+    )
     for issue_link in issue.fields.issuelinks:
         if issue_link.type.name == 'Blocks' and hasattr(issue_link, 'outwardIssue'):
-            issue_graph.add_link(issue.key, issue_link.outwardIssue.key)
+            if not is_closed(issue_link.outwardIssue):
+                issue_graph.add_link(issue.key, issue_link.outwardIssue.key)
 
+    if story_points:
+        total_points += int(story_points)
+        for label in issue.fields.labels:
+            total_points_per_label[label] += int(story_points)
+    else:
+        not_estimated_issues.append(issue)
+
+print(f'Total issues: {len(issues)}')
+print(f'Not estimated issues: {len(not_estimated_issues)}')
+print(f'Total points: {total_points}')
+for label, points in total_points_per_label.items():
+    print(f'{label} points: {points}')
 issue_graph.show()
